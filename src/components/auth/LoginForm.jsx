ba7612087e41d0787ce97c02f1ea576a91ed4b29
NieamulKabir <kabir000079@gmail.com>
2024-03-28 23:52:52 +0600

    input form create

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,11 +1,60 @@
-
+import { useForm } from "react-hook-form";
+import Field from "../common/Field";
 
 const LoginForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      {/* email input field  */}
+      <Field label="Email">
+        <input
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-200"
+          }`}
+          {...register("email", { required: "Email ID is Required" })}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </Field>
+      {/* password input field  */}
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Your password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${
+            errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+
+      {/* submit button  */}
+      <Field>
+        <button className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
+          Login
+        </button>
+      </Field>
+    </form>
+  );
 };
 
 export default LoginForm;
