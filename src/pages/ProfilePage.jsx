7b229ef18c3eed51a0a0798f31225d9fd6a2330b
NieamulKabir <kabir000079@gmail.com>
2024-04-02 22:56:57 +0600

    update

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 
 
 const ProfilePage = () => {
+    const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
     return (
         <div>
             <h1>Profile page</h1>
